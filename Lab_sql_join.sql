0500e76cc9b5eff89b2fc0df8f6c699ec913341a
grovyle12 <97551838+grovyle12@users.noreply.github.com>
2022-05-13 15:24:39 -0400

    Add files via upload

---
+++ Lab_sql_join.sql
@@ -0,0 +1,21 @@
+-- List the number of films per category.
+
+select * from 
+
+
+
+-- Display the first and the last names, as well as the address, of each staff member.
+
+
+
+
+-- Display the total amount rung up by each staff member in August 2005.
+
+
+
+-- List all films and the number of actors who are listed for each film.
+
+
+
+
+-- Using the payment and the customer tables as well as the JOIN command, list the total amount paid by each customer. List the customers alphabetically by their last names.
